ae12d964fefe0b5e9aacc792e2f87e6207371e47
Manuel <manuel.schmuki@gmail.com>
2021-09-13 16:05:38 +0200

    Style Progress section

--- src/components/TaskProgress.jsx
+++ src/components/TaskProgress.jsx
@@ -1,26 +1,33 @@
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ListContext } from "../App";
 import "./TaskProgress.scss";
 
 export default function TaskProgress() {
   const taskContext = useContext(ListContext);
-  const taskProgress = useMemo(
-    () =>
-      (taskContext.state.filter((t) => t.completed).length /
-        taskContext.state.length) *
-      100,
-    [taskContext.state]
-  );
+  const [finishedCount, setFinishedCount] = useState(0);
+  const [finishedPercentage, setFinishedPercentage] = useState(0);
+
+  useEffect(() => {
+    const newFinishedCount = taskContext.state.filter(
+      (t) => t.completed
+    ).length;
+    setFinishedCount(newFinishedCount);
+    setFinishedPercentage((newFinishedCount / taskContext.state.length) * 100);
+  }, [taskContext]);
 
   return (
     <div className="progress-bar">
-      <div
-        style={{
-          height: "100%",
-          width: taskProgress + "%",
-          backgroundColor: "blue",
-        }}
-      ></div>
+      <h1>Progress</h1>
+      <div className="progress-bar__container">
+        <div
+          style={{
+            height: "100%",
+            width: finishedPercentage + "%",
+            backgroundColor: "white",
+          }}
+        ></div>
+      </div>
+      <p className="progress-bar__counter">{finishedCount} completed</p>
     </div>
   );
 }
